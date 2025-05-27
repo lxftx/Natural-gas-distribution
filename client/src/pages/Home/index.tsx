6bda67134d95b63b617557111fbefeb3f60b7ba6
Oleg <olegtirex@mail.ru>
2025-05-28 00:27:02 +0500

    feat(Home): Add HomePage component that combines all layout sections

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import Header from "./components/sections/Header"
+import Nav from "./components/sections/Nav"
+import Section from "./components/sections/Section";
+import Footer from "./components/sections/Footer";
+
+function HomePage() {
+    return (
+        <>
+            <Header/>
+            <Nav/>
+            <Section/>
+            <Footer/>
+        </>
+    )
+}
+
+export default HomePage
