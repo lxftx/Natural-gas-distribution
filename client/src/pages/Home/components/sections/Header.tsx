9633054c3bbdb127d1b6e7a4c5dfe81764bb96af
Oleg <olegtirex@mail.ru>
2025-05-28 00:22:02 +0500

    feat(component): add Header for the Home page

---
+++ client/src/pages/Home/components/sections/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="h-100 color bg-black p-10">
+            <div className="text-4xl text-center font-thin text-white">
+                Распределение природного газа в группе доменных печей
+            </div>
+        </header>
+    )
+}
+
+export default Header
