f8c70af875ae44784b3f733690d55f355a6c0ae0
Oleg <olegtirex@mail.ru>
2025-05-31 19:28:41 +0500

    feat(API): add API method for input values

--- server/gas/views.py
+++ server/gas/views.py
@@ -1,7 +1,8 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from gas.serializers import CalculateSerializer
+from gas.serializers import CalculateSerializer, CalculatedSerializer
 from gas.services import GasDistributionService
+from gas.services import DefaultInputValues
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,20 +18,7 @@ class CalculateAPIView(APIView):
         operation_description="API метод расчета задачи распределения природного газа в группе доменных печей",
         request_body=CalculateSerializer,
         responses={
-            status.HTTP_200_OK: openapi.Response(
-                description="Оптимальное решение найдено",
-                examples={
-                    "application/json": {
-                        "objective": 123.123,
-                        "gas_distribution": [10.00, 20.00, 30.00, 40.00],
-                        "total_gas_consumption": 123.123,
-                        "total_coke_consumption": 123.123,
-                        "total_iron_production": 123.123,
-                        "sulfur_content": [10.00, 20.00, 30.00, 40.00],
-                        "status": "OPTIMAL"
-                    }
-                }
-            ),
+            status.HTTP_200_OK: CalculatedSerializer,
             status.HTTP_400_BAD_REQUEST: openapi.Response(
                 description="Оптимальное решение не найдено",
                 examples={
@@ -47,6 +35,10 @@ class CalculateAPIView(APIView):
             serializer.is_valid(raise_exception=True)
 
             result = GasDistributionService.calculate_distribution(serializer.validated_data)
+            serializer = CalculatedSerializer(data=result)
+            serializer.is_valid(raise_exception=True)
+
+            result = serializer.validated_data
             return Response(
                 data=result,
                 status=status.HTTP_200_OK
@@ -54,5 +46,42 @@ class CalculateAPIView(APIView):
         except ValueError as e:
             return Response(
                 data={"error": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+class DefaultInputValuesAPIView(APIView):
+    """
+    API метод получения входных значений по умолчанию
+    """
+    @swagger_auto_schema(
+        operation_summary="Получить значения по умолчанию",
+        operation_description="API метод получения входных значений по умолчанию",
+        responses={
+            status.HTTP_200_OK: CalculateSerializer,
+            status.HTTP_400_BAD_REQUEST: openapi.Response(
+                description="Ошибка получения входных данных",
+                examples={
+                    "application/json": {
+                        "error": "<Ошибка>"
+                    }
+                }
+            )
+        }
+    )
+
+    def get(self, *args, **kwargs):
+        try:
+            values = DefaultInputValues.get_default_values()
+            serializer = CalculateSerializer(data=values)
+            serializer.is_valid(raise_exception=True)
+
+            result = serializer.validated_data
+            return Response(
+                data=result,
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                data={"error": "Ошибка при получений входных значений по умолчанию."},
                 status=status.HTTP_400_BAD_REQUEST
             )
