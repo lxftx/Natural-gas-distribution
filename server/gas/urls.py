453fc1fca26e52111a8ed52f32ff3b414fb8bcb0
Oleg <olegtirex@mail.ru>
2025-06-02 20:22:30 +0500

    feat(urls): added router to API method history

--- server/gas/urls.py
+++ server/gas/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from gas.views import CalculateAPIView, DefaultInputValuesAPIView
+from rest_framework.routers import DefaultRouter
+
+from gas.views import (CalculateAPIView, DefaultInputValuesAPIView,
+                       HistoryViewSet)
 
 app_name = 'gas'
+
+router = DefaultRouter()
+router.register(r'history', HistoryViewSet)
+
 
 urlpatterns = [
     path("calculate/", CalculateAPIView.as_view(), name="calculate"),
     path("default/", DefaultInputValuesAPIView.as_view(), name="default")
-]
+] + router.urls
