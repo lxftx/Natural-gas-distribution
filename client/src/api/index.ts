d95453b5721a1ba9e11917366db3c6a876355e24
Oleg <olegtirex@mail.ru>
2025-06-07 02:32:13 +0500

    feat(API): add request to API server

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { FurnaceData, HistoryDataPost } from "@/pages/Solver/components/types.ts";
+import { AuthError } from "@/lib/types"
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -8,25 +9,75 @@ const API = axios.create({
   timeout: 5000, // Таймаут запроса
 });
 
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.withCredentials = true;  // Важно для отправки cookies
+  return config;
+});
+
 export default {
     // Метод для расчета
-    calculate(data: FurnaceData) {
-        return API.post("calculate/", data);
+    async calculate(data: FurnaceData) {
+        return await API.post("gas/calculate/", data);
     },
     // Метод для входных данных
-    defaultInputValues() {
-      return API.get("default/")
+    async defaultInputValues() {
+      return await API.get("gas/default/")
     },
     // Метод для получения истории
-    getHistory() {
-      return API.get("history/")
+    async getHistory() {
+      return await API.get("gas/history/")
     },
     // Метод для добавления истории
-    addHistory(data: HistoryDataPost) {
-      return API.post("history/", data);
+    async addHistory(data: HistoryDataPost) {
+      return await API.post("gas/history/", data);
     },
     // Метод для удаления истории
-    deleteHistory(id: number) {
-      return API.delete(`history/${id}/`)
+    async deleteHistory(id: number) {
+      return await API.delete(`gas/history/?id=${id}`)
+    },
+    async infoUser() {
+      return await API.get("/user/info/")
+    },
+    async loginUser(email: string, password: string): Promise<string> {
+      try {
+        const response = await API.post("user/login/", { email, password })
+        return response.data.access
+      } catch (error: any) {
+        const message = error.response?.data?.error || "Ошибка авторизации"
+        throw new AuthError(message, error.response?.status)
+      }
+    },
+    async registerUser(first_name: string, last_name: string, email: string, password: string): Promise<string> {
+      try {
+        const response = await API.post("user/login/", {email, first_name, last_name, password})
+
+        return response.data.access
+      } catch (error: any) {
+        const message = error.response?.data?.error || "Ошибка регистрации"
+        throw new AuthError(message, error.response?.status)
+      }
+    },
+    async refreshAccessToken(): Promise<string> {
+      try {
+        const response = await API.post("user/token/refresh/")
+
+        return response.data.access
+      } catch (error: any) {
+        const message = error.response?.data?.error || "Не удалось обновить токен"
+        throw new AuthError(message, error.response?.status)
+      }
+    },
+    async logout() {
+      try {
+        await API.post("user/logout/")
+      } catch (error: any) {
+        const message = error.response?.data?.error || "Ошибка выхода из системы"
+        throw new AuthError(message, error.response?.status)
+      }
     }
 }
