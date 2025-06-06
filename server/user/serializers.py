d48a7e3faf4108b7340bb5a08505324e8257a0d4
Oleg <olegtirex@mail.ru>
2025-06-07 02:18:19 +0500

    feat(serializer): add user info serializer

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -7,6 +7,11 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from user.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name"]
+
 class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
