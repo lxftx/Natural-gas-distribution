f3a8bd52c1c4c4539d7ddde325de2f54cf9a48bb
Oleg <olegtirex@mail.ru>
2025-06-08 00:11:18 +0500

    refactor(serializer): improve readability with OOP structure

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -7,17 +7,32 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["email", "first_name", "last_name"]
+class UserBaseSerializer(serializers.ModelSerializer):
+    """
+    Базовый сериализатор для модели User.
+    """
 
-class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "first_name", "last_name", "password"]
-        # Пароль не будет возвращаться в ответе
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = []
+        extra_fields = {
+            "email": {
+                "min_length": 1,
+                "max_length": 255,
+                "help_text": "Email адрес",
+            },
+            "first_name": {
+                "min_length": 1,
+                "max_length": 64,
+                "help_text": "Имя пользователя",
+            },
+            "last_name": {
+                "min_length": 1,
+                "max_length": 64,
+                "help_text": "Фамилия пользователя",
+            },
+        }
+        read_only_fields = ["created_at", "is_active", "is_staff"]
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
@@ -28,6 +43,24 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         # Стандартная валидация пароля Django
         validate_password(value)
         return value
+
+class UserDetailSerializer(UserBaseSerializer):
+    """
+    Сериализатор модели User для детального просмотра расчетов.
+    """
+
+    class Meta(UserBaseSerializer.Meta):
+        fields = UserBaseSerializer.Meta.fields + ["email", "first_name", "last_name"]
+
+class UserCreateSerializer(UserBaseSerializer):
+    """
+    Сериализатор модели User для создания расчета. 
+    """
+    
+    class Meta(UserBaseSerializer.Meta):
+        fields = UserBaseSerializer.Meta.fields + ["email", "first_name", "last_name", "password"]
+        # Пароль не будет возвращаться в ответе
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
